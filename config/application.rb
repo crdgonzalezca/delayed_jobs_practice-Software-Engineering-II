61317f4c710e64769c4b7019e2fc2dc088dff393
Cristian David Gonzalez Carrillo <crdgonzalezca@unal.edu.co>
2018-05-22 01:02:30 +0000

    Delayed jobs example

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module DelayedJobs
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+    
+    config.active_job.queue_adapter = :delayed_job
+    
+    config.action_mailer.default_url_options = {host: 'example.com'}
+    
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
